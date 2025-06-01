bd47d0eadcd5216e9c62dcd58693e7ec41ce2b77
deaamanii <dm68549@ubt-uni.net>
2025-06-02 00:19:19 +0200

    Add user dashboard components and access control template

--- backend/Core/Dtos/Auth/UserInfoResult.cs
+++ backend/Core/Dtos/Auth/UserInfoResult.cs
@@ -2,11 +2,11 @@
 {
     public class UserInfoResult
     {
-        public string Id { get; set;}
-        public string FirstName { get; set; }   
-        public string LastName { get; set;}
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string UserName { get; set; }
-        public string Email { get; set;}
+        public string Email { get; set; }
         public string Gender { get; set; }
         public string Address { get; set; }
         public DateTime CreatedAt { get; set; }
