53b2e64ed890cb85a0c42faae2bb932327e7cb07
Ylldrit Qallakaj <qallakajylldrit9@gmail.com>
2025-06-23 13:09:04 +0200

    update git ignore

---
+++ backend/Core/Settings/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+namespace Core.Settings
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+    }
+} 
